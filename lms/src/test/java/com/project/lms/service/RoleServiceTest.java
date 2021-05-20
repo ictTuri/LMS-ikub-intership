d9f30a70fcd4f6fcc1a2e72125c35fd753eecee2
Artur Molla <mollaartur13@gmail.com>
2021-05-20 20:39:31 +0200

    Service tests

--- lms/src/test/java/com/project/lms/service/RoleServiceTest.java
+++ lms/src/test/java/com/project/lms/service/RoleServiceTest.java
@@ -1,14 +1,41 @@
 package com.project.lms.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import com.project.lms.model.RoleEntity;
+import com.project.lms.repository.RoleRepository;
+import com.project.lms.service.impl.RoleServiceImpl;
+import com.project.lms.utils.RoleUtil;
+
+@SpringBootTest
 class RoleServiceTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	@InjectMocks
+	private RoleServiceImpl roleService;
 
+	@Mock
+	private RoleRepository roleRepository;
+	
+	@Test
+	void givenRoleList_whenGetThem_validateSize() {
+		List<RoleEntity> roles = new ArrayList<>();
+		roles.add(RoleUtil.adminRole());
+		roles.add(RoleUtil.secretaryRole());
+		roles.add(RoleUtil.studentRole());
+
+		when(roleRepository.getAllRoles()).thenReturn(roles);
+		int size = roleService.showAllRoles().size();
+		assertEquals(3, size);
+		verify(roleRepository).getAllRoles();
+	}
 }
--- lms/src/test/java/com/project/lms/service/UserRoleServiceTest.java
+++ lms/src/test/java/com/project/lms/service/UserRoleServiceTest.java
@@ -1,14 +1,82 @@
 package com.project.lms.service;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import com.project.lms.dto.UserRoleCreateUpdateDto;
+import com.project.lms.dto.UserRoleDto;
+import com.project.lms.exception.ObjectIdNotFound;
+import com.project.lms.model.UserRoleEntity;
+import com.project.lms.repository.UserRoleRepository;
+import com.project.lms.service.impl.UserRoleServiceImpl;
+import com.project.lms.utils.RoleUtil;
+import com.project.lms.utils.UserUtil;
+
+@SpringBootTest
 class UserRoleServiceTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
 
+	@InjectMocks
+	private UserRoleServiceImpl userRoleService;
+	
+	@Mock
+	private UserRoleRepository userRoleRepository;
+	
+	
+	@Test
+	void givenUserRoleList_whenGetThem_validateSize() {
+		List<UserRoleEntity> userRoles = new ArrayList<>();
+		UserRoleEntity userRoleOne = new UserRoleEntity();
+		userRoleOne.setRole(RoleUtil.adminRole());
+		userRoleOne.setUser(UserUtil.userAdmin());
+		userRoles.add(userRoleOne);
+
+		when(userRoleRepository.getAllUserRoles()).thenReturn(userRoles);
+		int size = userRoleService.getAllUserRole().size();
+		
+		assertEquals(1, size);
+		verify(userRoleRepository).getAllUserRoles();
+	}
+	
+	@Test
+	void givenUserRole_whenUpdate_thenThrowsException() {
+		UserRoleCreateUpdateDto userRoleToUpdate = new UserRoleCreateUpdateDto();
+		userRoleToUpdate.setRole("ADMIN");
+		userRoleToUpdate.setUsername("admin");
+		
+		UserRoleEntity userRoleOne = new UserRoleEntity();
+		userRoleOne.setRole(RoleUtil.adminRole());
+		userRoleOne.setUser(UserUtil.userAdmin());
+		long id = 1;
+		
+		Mockito.when(userRoleRepository.updateUserRole(userRoleOne)).thenReturn(userRoleOne);
+		
+		assertThrows(ObjectIdNotFound.class, ()->{userRoleService.updateUserRole(id, userRoleToUpdate);});
+	}
+	
+	@Test
+	void givenUserRole_whenGet_thenValidateUserAndRole() {
+		UserRoleEntity userRoleOne = new UserRoleEntity();
+		userRoleOne.setRole(RoleUtil.adminRole());
+		userRoleOne.setUser(UserUtil.userAdmin());
+		long id = 1;
+		
+		Mockito.when(userRoleRepository.getUserRoleById(id)).thenReturn(userRoleOne);
+		UserRoleDto userRoleToGet = userRoleService.getUserRoleById(id);
+		
+		assertNotNull(userRoleToGet);
+		assertEquals(userRoleToGet.getRole().getName(), userRoleOne.getRole().getName());
+		assertEquals(userRoleToGet.getUser().getUsername(), userRoleOne.getUser().getUsername());
+	}
+	
 }
